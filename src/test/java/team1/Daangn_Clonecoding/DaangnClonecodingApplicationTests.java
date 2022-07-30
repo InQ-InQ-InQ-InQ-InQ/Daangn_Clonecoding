4d37a238bb10baf60f737559a486a94f32ee936b
Sanghyun Yi <njsharry32@gmail.com>
2022-07-30 23:49:32 +0900

    frontend init

---
+++ src/test/java/team1/Daangn_Clonecoding/DaangnClonecodingApplicationTests.java
@@ -0,0 +1,13 @@
+package team1.Daangn_Clonecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaangnClonecodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
