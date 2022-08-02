9fe8b9796ef902932f0d7bf84ea45c566f97d50d
eunchannam <namhm23@gmail.com>
2022-08-02 15:48:19 +0900

    explain 변수명 explains로 수정

--- src/main/java/team1/Daangn_Clonecoding/domain/posting/Posting.java
+++ src/main/java/team1/Daangn_Clonecoding/domain/posting/Posting.java
@@ -4,29 +4,27 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Posting {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
 
+    @Enumerated(EnumType.STRING)
     private PostingType postingType;
 
+    @Enumerated(EnumType.STRING)
     private Category category;
 
     private String productName;
 
     private String productPrice;
 
-    private String explain;
+    private String explains;
 }
