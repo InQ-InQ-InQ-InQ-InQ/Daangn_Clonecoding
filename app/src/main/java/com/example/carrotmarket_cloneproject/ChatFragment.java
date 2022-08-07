44c0d04ce744ac50c59ca19a6699c6509b20572d
최주현 <78858272+rnawlsgk@users.noreply.github.com>
2022-08-07 23:11:54 +0900

    init

---
+++ app/src/main/java/com/example/carrotmarket_cloneproject/ChatFragment.java
@@ -0,0 +1,33 @@
+package com.example.carrotmarket_cloneproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class ChatFragment extends Fragment {
+
+    public ChatFragment() {}
+
+
+    public static ChatFragment newInstance() {
+        ChatFragment fragment = new ChatFragment();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+}
