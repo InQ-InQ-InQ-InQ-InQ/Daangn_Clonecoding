02b6bedad23c95c2d484cd4a1fb28d9d6de35e89
rnawslgk <hamo35301@gmail.com>
2022-08-08 15:21:37 +0900

    chat-UI chat, chat list UI

--- app/src/main/java/com/example/carrotmarket_cloneproject/Activity_chat.java
+++ app/src/main/java/com/example/carrotmarket_cloneproject/Activity_chat.java
@@ -1,13 +1,17 @@
 package com.example.carrotmarket_cloneproject;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity_chat extends AppCompatActivity {
+    private Button btn_nickname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_layout);
+
     }
 }
