22118bf4194e7dc9ae6ecb0616773e4fa3a7312b
rnawslgk <hamo35301@gmail.com>
2022-08-08 20:40:13 +0900

    chat-activity-1 Activity_chat 기본작업

--- app/src/main/java/com/example/carrotmarket_cloneproject/Activity_chat.java
+++ app/src/main/java/com/example/carrotmarket_cloneproject/Activity_chat.java
@@ -2,16 +2,27 @@ package com.example.carrotmarket_cloneproject;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity_chat extends AppCompatActivity {
     private Button btn_nickname;
+    private ImageButton imgbtn_back, imgbtn_call, imgbtn_chat_menu, imgbtn_plus, imgbtn_chat_send;
+    private EditText ed_chat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat_layout);
+
+        btn_nickname = findViewById(R.id.button_nickname);
+        imgbtn_back = findViewById(R.id.button_back);
+        imgbtn_call = findViewById(R.id.button_call);
+        imgbtn_chat_menu = findViewById(R.id.button_chat_menu);
+        imgbtn_plus = findViewById(R.id.button_plus);
+        imgbtn_chat_send = findViewById(R.id.button_chat_send);
 
     }
 }
